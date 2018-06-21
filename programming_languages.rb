f42696951d30fcbe1d08cd104c95f4b1223109a0
Yan Wang <wangyan6783@gmail.com>
2018-06-21 13:55:50 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each { |style, language|
+    language.each { |name, attributes|
+      if new_hash[name] == nil
+        attributes[:style] = [style]
+        new_hash[name] = attributes
+      else
+        new_hash[name][:style] << style
+      end
+    }
+  }
+  new_hash
 end
