74c7cc8b3963c35cb0826db13c4d478a856ddd15
Yan Wang <wangyan6783@gmail.com>
2018-06-21 13:55:09 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,17 +1,13 @@
 def reformat_languages(languages)
   new_hash = {}
   languages.each { |style, language|
-  
-    # language[:style] = [] << style
     language.each { |name, attributes|
       if new_hash[name] == nil
         attributes[:style] = [style]
         new_hash[name] = attributes
       else
-        
+        new_hash[name][:style] << style
       end
-      
-      
     }
   }
   new_hash
